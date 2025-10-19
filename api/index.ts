a9d3e52a7071b96f8f682e202309b248d4fc7b1c
eriks.freimanis <eriks,.freimanis@abcsoftware.lv>
2025-10-19 18:47:06 +0300

    test

--- api/index.ts
+++
@@ -1,5 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import app from '../server';
-
-export default (req: VercelRequest, res: VercelResponse) => (app as any)(req, res);
-
